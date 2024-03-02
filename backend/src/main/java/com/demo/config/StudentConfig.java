e863e587dfdf0f4286f8b6bba02cbfe30da3a5a7
Iuliia Rudi <connection454@gmail.com>
2024-03-02 13:12:46 -0500

    add StudentConfig properties

---
+++ backend/src/main/java/com/demo/config/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.demo.config;
+
+//import src.main.java.com.demo.entities.Student;
+//import src.main.java.com.demo.repositories.StudentRepository;
+import com.demo.entities.Student;
+import com.demo.repositories.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    // Spring runs CommandLineRunner bean when Spring Boot App starts
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student piter = new Student(1L, "Piter", "piter.smithon@gmail.com",
+                    LocalDate.of(2005, Month.MAY, 17));
+
+            Student alex = new Student(1L, "Alex", "alex.spenser@gmail.com",
+                    LocalDate.of(2003, Month.NOVEMBER, 25));
+            repository.saveAll(
+                    List.of(piter, alex)
+            );
+        };
+    }
+}
