503f15e42e137b227d202830d511a11d74ac6dfd
Iuliia Rudi <connection454@gmail.com>
2024-03-03 09:24:00 -0500

    add main class

---
+++ backend/src/main/java/com/demo/SpringBootStudentApplication.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudentApplication.class, args);
+    }
+
+}
