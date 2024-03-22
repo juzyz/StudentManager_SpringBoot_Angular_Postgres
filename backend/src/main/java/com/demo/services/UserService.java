b850a41fa9404c39cb388c4fac52be84286ac04f
Iuliia Rudi <connection454@gmail.com>
2024-03-22 17:33:40 -0400

    add UserRepository class

---
+++ backend/src/main/java/com/demo/services/UserService.java
@@ -0,0 +1,28 @@
+package com.demo.services;
+
+//import com.demo.entities.StudentUser;
+import com.demo.entities.StudentUser;
+import com.demo.exeptions.UserNotFoundException;
+import com.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public  User findUserByEmail(String username) {
+        Optional <StudentUser> test = userRepository.findUserByUsername(username);
+
+        StudentUser studentUser = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("studentUser with email " + username + " does not exist"));
+        return new User(studentUser.getUsername(), studentUser.getPassword(), Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))) ;
+    }
+
+}
