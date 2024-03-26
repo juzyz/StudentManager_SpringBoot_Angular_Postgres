f2262fabb9b1402f28dab9534c8a2b42783e034c
Iuliia Rudi <connection454@gmail.com>
2024-03-25 22:56:22 -0400

    added UserRepository

---
+++ backend/src/main/java/com/demo/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.demo.repositories;
+
+import com.demo.entities.StudentUser;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+//@Repository
+public interface UserRepository extends JpaRepository<StudentUser, Long> {
+    @Query("SELECT * FROM student_user u WHERE u.username = ?1")
+    Optional<StudentUser> findUserByUsername(String username);
+
+}
