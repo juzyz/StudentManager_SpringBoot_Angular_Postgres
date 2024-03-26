692855c6a31788484c54f0ded531af447c64aae5
Iuliia Rudi <connection454@gmail.com>
2024-03-25 22:55:30 -0400

    updated UserDoa class

--- backend/src/main/java/com/demo/repositories/UserDoa.java
+++ backend/src/main/java/com/demo/repositories/UserDoa.java
@@ -1,46 +1,64 @@
 package com.demo.repositories;
 
+//import org.springframework.security.core.userdetails.StudentUser;
+
+
+import com.demo.entities.StudentUser;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
-@Repository
+//@Repository
 public class UserDoa {
-    private static final User user1 = new User(
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(UserRepository repository) {
+//        return args -> {
+//            StudentUser user1 = new StudentUser(
+//                    "admin.mail@gmail.com",
+//                    "123",
+//                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
+//            );
+//
+//            StudentUser user2 = new StudentUser(
+//                    "user.mail@gmail.com",
+//                    "456",
+//                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+//            );
+//            repository.saveAll(
+//                    List.of(user1, user2)
+//            );
+//        };
+//    }
+    private static final StudentUser user1 = new StudentUser(
             "admin.mail@gmail.com",
-            "123",
-            Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
-    );
+            "123"
 
-    private static final User user2 = new User(
-            "user.mail@gmail.com",
-            "456",
-            Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
     );
-    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
-            user1, user2
-    );
+//
+//    private static final StudentUser user2 = new StudentUser(
+//            "user.mail@gmail.com",
+//            "456",
+//            Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+//    );
+//    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+//            user1, user2
+//    );
 
-    public UserDetails findUserByEmail(String email) {
-        if (user1.getPassword() == null) {
-            return new User(
-                    "admin.mail@gmail.com",
-                    "123",
-                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
-            );
-        }
-        return APPLICATION_USERS
-                .stream()
-                .filter(u -> u.getUsername().equals(email))
-                .findFirst()
-                .orElseThrow(() -> new UsernameNotFoundException("No user was founded"));
-    }
+//    public StudentUser findUserByEmail(String email) {
+////        if (user1.getPassword() == null) {
+////            return new StudentUser(
+////                    "admin.mail@gmail.com",
+////                    "123",
+////                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
+////            );
+////        }
+//        return APPLICATION_USERS
+//                .stream()
+//                .filter(u -> u.getUsername().equals(email))
+//                .findFirst()
+//                .orElseThrow(() -> new UsernameNotFoundException("No user was founded"));
+//    }
 }
 
 
