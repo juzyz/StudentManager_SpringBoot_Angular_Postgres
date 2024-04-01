0c23224a7885be406db0f30177ceacc198550fa9
Iuliia Rudi <connection454@gmail.com>
2024-03-31 22:48:43 -0400

    add new components to app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,45 +6,60 @@ import {HttpClientModule} from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './modules/login/login-component/login.component';
 import {MatInputModule} from '@angular/material/input';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StudentComponent } from './modules/student/student-component/student.component';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
- 
+
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatCardModule} from '@angular/material/card'; 
+import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table' ;
+import { MatIconModule } from '@angular/material/icon';
 import { AuthGuard } from './authentication/guards/authGard/auth.guard';
+// import { CommonModule } from '@angular/common';
+import { StudentListComponent } from './modules/student/student-list/student-list.component';
+import { CreateStudentComponent } from './modules/student/create-student/create-student.component';
+import { UpdateStudentComponent } from './modules/student/update-student/update-student.component';
+import { AttendanceReportComponent } from './modules/reports/attendance-report/attendance-report.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    StudentComponent
+    StudentComponent,
+    StudentListComponent,
+    CreateStudentComponent,
+    UpdateStudentComponent,
+    AttendanceReportComponent
+
   ],
-  imports: [     
+  imports: [
     BrowserModule,
-    AppRoutingModule,   
-    HttpClientModule,   
+    AppRoutingModule,
+    HttpClientModule,
     MatInputModule,
-    FormsModule, 
-    ReactiveFormsModule, 
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatSlideToggleModule,
     BrowserAnimationsModule,
     MatSnackBarModule,
-
     MatDatepickerModule,
     MatFormFieldModule,
     MatNativeDateModule,
     MatProgressSpinnerModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    // CommonModule
   ],
   providers: [
     provideClientHydration(),
