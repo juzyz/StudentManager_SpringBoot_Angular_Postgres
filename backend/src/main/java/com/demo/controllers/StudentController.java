f6609089d9bdae7ebcffd0b72e99adbcbd3a9338
Iuliia Rudi <connection454@gmail.com>
2024-04-02 21:08:22 -0400

    updated Student Controller

--- backend/src/main/java/com/demo/controllers/StudentController.java
+++ backend/src/main/java/com/demo/controllers/StudentController.java
@@ -8,8 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
-@RequestMapping( path="api/v1/student")
+@RequestMapping(path = "api/v1/student")
 @CrossOrigin(origins = "http://localhost:4200")
 //@CrossOrigin(  "*")
 public class StudentController {
@@ -22,36 +23,41 @@ public class StudentController {
     }
 
     @GetMapping
-    public List<Student> getStudent() {
+    public List<Student> getAllStudent() {
         return studentService.getAllStudent();
     }
 
+    @GetMapping(path = "{studentId}")
+    public Student getStudentById(@PathVariable("studentId") Long studentId) {
+        return studentService.getStudentById(studentId);
+    }
+
     @PostMapping
-    public void registerNewStudent(@RequestBody Student student){
-          studentService.addNewStudent(student);
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
     }
 
-    @DeleteMapping(path="{studentId}")
-    public void deleteStudent(@PathVariable("studentId") Long studentId){
-        studentService.deleteStudent (studentId);
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
     }
 
-    @PutMapping(path="{studentId}")
-    public  void updateStudent(@PathVariable("studentId") Long studentId,
-                               @RequestParam(required = false) String name,  // sent parameter via URL Parameters
-                               @RequestParam(required = false) String email,
-                               @RequestBody Student student  // or parse parameters from json of request body
-                               ){
-        studentService.updateStudent(studentId, name, email);
+    @PutMapping
+    public void updateStudent(@RequestBody Student student  // or parse parameters from json of request body
+    ) {
+        if (student.getId() != null && student.getName() != null && student.getEmail() != null) {
+            studentService.updateStudent(student.getId(), student.getName(), student.getEmail());
+        }
+
     }
 
     @GetMapping("/say-hello")
-    public ResponseEntity <String> sayHello (){
+    public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from API");
     }
 
-    @GetMapping ("/say-goodbye")
-    public ResponseEntity <String> sayGoodbye (){
+    @GetMapping("/say-goodbye")
+    public ResponseEntity<String> sayGoodbye() {
         return ResponseEntity.ok("Goodbye and see you letter");
     }
 }
