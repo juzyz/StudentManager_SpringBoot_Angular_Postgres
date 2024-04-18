ba149cee72bb36efdb960a49d16c0d99dd146d66
Iuliia Rudi <connection454@gmail.com>
2024-04-17 20:45:43 -0400

    move creation of attendance report to separate repository

--- backend/src/main/java/com/demo/controllers/reports/AttendanceTrackerController.java
+++ backend/src/main/java/com/demo/controllers/reports/AttendanceTrackerController.java
@@ -3,65 +3,43 @@ package com.demo.controllers.reports;
 
 import com.demo.entities.reports.AttendanceTrack;
 import com.demo.entities.Student;
+import com.demo.services.AttendTrackService;
 import com.demo.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/report")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AttendanceTrackerController {
-    HashMap<String, LinkedList<AttendanceTrack>> reportData = new HashMap<>();
-
-//    @Bean
-//    CommandLineRunner reportCommandLineRunner() {
-//        return args -> {
-//            AttendanceTrack aTrack1 = new AttendanceTrack(
-//                    1l,
-//                    true
-//            );
-//
-//            AttendanceTrack aTrack2 = new AttendanceTrack(
-//                    1l,
-//                    false
-//            );
-//
-//            AttendanceTrack aTrack3 = new AttendanceTrack(
-//                    2l,
-//                    true
-//            );
-//            AttendanceTrack aTrack4 = new AttendanceTrack(
-//                    2l,
-//                    false
-//            );
-//
-//
-//            LinkedList attendanceList1 = new LinkedList<>();
-//            attendanceList1.addAll(List.of(aTrack1, aTrack3));
-//            reportData.put("12-12-2023", attendanceList1);
-//            LinkedList attendanceList2 = new LinkedList<>();
-//            attendanceList2.addAll(List.of(aTrack2));
-//            reportData.put("12-11-2023", attendanceList2);
-//        };
-//    }
+    HashMap<LocalDate, LinkedList<AttendanceTrack>> reportData = new HashMap<>();
 
     @Autowired
+    AttendanceTrackerController(StudentService studentService, AttendTrackService attendTrackService) {
+        this.studentService = studentService;
+        this.attendTrackService = attendTrackService;
+    }
+
+
     StudentService studentService;
+    AttendTrackService attendTrackService;
 
     @PostMapping("addTrack/{date}")
-    public void addNewAttendanceTrack(@PathVariable String date, @RequestBody AttendanceTrack attendanceTrack) {
+    public void addNewAttendanceTrack(@PathVariable LocalDate date, @RequestBody AttendanceTrack attendanceTrack) {
         LinkedList<AttendanceTrack> attendanceList = reportData.get(date);
         if (attendanceList == null) {
             attendanceList = new LinkedList<>();
             reportData.put(date, attendanceList);
-        }  // need to add check is AttendanceTrack is already added for given empId or re-write to use HashSet
+        }  // need to add check if AttendanceTrack is already added for given empId or re-write to use HashSet
         attendanceList.add(attendanceTrack);
     }
 
@@ -70,28 +48,26 @@ public class AttendanceTrackerController {
         reportData.clear();
     }
 
-    @GetMapping("getPresentStudents/{date}")
-    public List<Student> getPresentStudents(@PathVariable String date) {
-        int df =5;
-//        return reportData.get(date).stream().filter(aTrack -> aTrack.getIsPresent()).collect(Collectors.toList());
+    @GetMapping("getPresentStudents/{stringDate}")
+    public List<Student> getPresentStudents(@PathVariable String stringDate) {
+        LocalDate date = prepareData(stringDate);
         List<AttendanceTrack> atList = reportData.get(date).stream()
                 .filter(aTrack -> aTrack.getIsPresent())
                 .collect(Collectors.toList());
         return atList.stream()
-//                .map(atTr -> studentService.getStudentById(atTr.getStudentId()))
                 .map(atTr -> studentService.getStudentById(atTr.getId().getStudentId()))
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("getAbsentStudents/{date}")
-    public List<Student> getAbsentStudents(@PathVariable String date) {
+    @GetMapping("getAbsentStudents/{stringDate}")
+    public List<Student> getAbsentStudents(@PathVariable String stringDate) {
+        LocalDate date = prepareData(stringDate);
         List<AttendanceTrack> atList = reportData.get(date).stream()
                 .filter(aTrack -> !aTrack.getIsPresent())
                 .collect(Collectors.toList());
         return atList.stream()
                 .map(atTr -> studentService.getStudentById(atTr.getId().getStudentId()))
                 .collect(Collectors.toList());
-//        return null;
     }
 
     @PutMapping("updateAbsentEmployees/{date}/")
@@ -102,4 +78,30 @@ public class AttendanceTrackerController {
                 .findFirst()
                 .ifPresent(updatedTrack -> updatedTrack.setIsPresent(attendanceTrack.getIsPresent()));
     }
+
+
+    private LocalDate prepareData(String stringDate) {
+        if (reportData.isEmpty()) {
+            retrievedDataFromDB();
+        }
+        DateTimeFormatter df = new DateTimeFormatterBuilder()
+                .parseCaseInsensitive()
+                .appendPattern("MM-dd-yyyy")
+                .toFormatter(Locale.ENGLISH);
+        return LocalDate.parse(stringDate, df);
+    }
+
+    private void retrievedDataFromDB() {
+        List<AttendanceTrack> allAttendTracks = attendTrackService.getAllAttendTracks();
+        for (AttendanceTrack track : allAttendTracks) {
+            LinkedList<AttendanceTrack> trackList = reportData.get(track.getId().getDate());
+            if (reportData.get(track.getId().getDate()) == null) {
+                trackList = new LinkedList<>();
+                reportData.put(track.getId().getDate(), trackList);
+            }
+            trackList.add(track);
+        }
+    }
+
+
 }
