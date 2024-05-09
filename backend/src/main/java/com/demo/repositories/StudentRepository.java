ab72fb7d620f6bf66aa73f7d4e6edf579547ea71
Iuliia Rudi <connection454@gmail.com>
2024-05-09 15:08:54 -0400

    updated StudentRepository

--- backend/src/main/java/com/demo/repositories/StudentRepository.java
+++ backend/src/main/java/com/demo/repositories/StudentRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT s FROM student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
+
+    @Query("SELECT s FROM student s WHERE s.average_mark BETWEEN ?1 and ?2")
+    Optional<List<Student>> getStudentsByAverageMarkBetween(Double minMark, Double maxMark);
 
 }
