fed4171b8b5998ea3df391d8e957d5b6545e6c8a
Iuliia Rudi <connection454@gmail.com>
2024-05-11 02:05:07 -0400

    add generating more students in StudentConfig

--- backend/src/main/java/com/demo/config/StudentConfig.java
+++ backend/src/main/java/com/demo/config/StudentConfig.java
@@ -4,12 +4,15 @@ package com.demo.config;
 //import src.main.java.com.demo.repositories.StudentRepository;
 import com.demo.entities.Student;
 import com.demo.repositories.StudentRepository;
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -21,13 +24,28 @@ public class StudentConfig {
     CommandLineRunner studentCommandLineRunner(StudentRepository repository) {
         return args -> {
             Student piter = new Student(1L, "Piter", "piter.smithon@gmail.com",
-                    LocalDate.of(2005, Month.MAY, 17));
+                    LocalDate.of(2005, Month.MAY, 17), 75.5);
 
             Student alex = new Student(2L, "Alex", "alex.spenser@gmail.com",
-                    LocalDate.of(2003, Month.NOVEMBER, 25));
+                    LocalDate.of(2003, Month.NOVEMBER, 25), 65.6);
             repository.saveAll(
                     List.of(piter, alex)
             );
+            repository.saveAll(generateStudents ());
         };
+    }
+
+    private List<Student> generateStudents (){
+        Faker faker = new Faker();
+        List<Student> studentList = new ArrayList<>();
+        for (long i=3; i < 33; i++){
+            Student student = new Student(i, faker.name().name(), faker.internet().emailAddress(),
+                    faker.date().birthday(18, 100).toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+                    faker.random().nextInt(10000)/100.0);
+            studentList.add(new Student(i, faker.name().name(), faker.internet().emailAddress(),
+                    faker.date().birthday(18, 100).toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+                    faker.random().nextInt(10000)/100.0));
+        }
+        return studentList;
     }
 }
